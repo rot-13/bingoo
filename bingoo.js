4056a3702f64f0c36a6f4e067c7099a0fed89c35
Gilad Goldberg <giladgo@gmail.com>
2014-01-29 17:48:45 +0200

    Initial Code

---
+++ bingoo.js
@@ -0,0 +1,14 @@
+chrome.tabs.onUpdated.addListener(
+    function(tabId, changeInfo, tab) {
+        var url = tab.url;
+        if (url.indexOf('https://www.bing.com/maps/default.aspx') == 0 && changeInfo.status == 'loading') {
+            var rtpIndex = url.indexOf("rtp=");
+            if (rtpIndex >= 0) {
+                var rtp = url.substr(rtpIndex + 4);
+                rtp = rtp.substr(0, rtp.indexOf('&'));
+                var address = rtp.substr(rtp.lastIndexOf('_') + 1);
+                chrome.tabs.update(tabId, {url: 'https://www.google.com/maps/preview/place/' + address});
+            }
+        }
+    }
+)
